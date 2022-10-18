93dae5892890c5025810c12291143c8608aab199
Vlad84 <vlad_zaycev_1984@mail.ru>
2022-10-18 18:13:50 +0300

    является ли число четным

---
+++ Domashka1_6_chetnoe li chislo/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт является ли оно чётным
+Console.WriteLine("Введите число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+if(num1 % 2 == 0)
+{
+    Console.WriteLine("число является четным");   
+}
+else
+{
+    Console.WriteLine("число является нечетным");
+}
