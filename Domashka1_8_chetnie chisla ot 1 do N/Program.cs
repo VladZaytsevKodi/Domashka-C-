652b2204e1ede94c7f56c2e7bbaf03958f996911
Vlad84 <vlad_zaycev_1984@mail.ru>
2022-10-18 19:51:17 +0300

    все четные числа от 1 до N

---
+++ Domashka1_8_chetnie chisla ot 1 do N/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все четные числа от 1 до N
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+for(int i = 1; i < n + 1; i++)
+{
+    if(i % 2 == 0)
+    {
+    Console.WriteLine(i);   
+    }
+}
