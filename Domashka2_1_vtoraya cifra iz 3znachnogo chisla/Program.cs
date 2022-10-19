f6e255a4360449e63828586a9d8fedcbe736e774
Vlad84 <vlad_zaycev_1984@mail.ru>
2022-10-19 15:24:55 +0300

    вторая цифла из 3хзначного числа

---
+++ Domashka2_1_vtoraya cifra iz 3znachnogo chisla/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает трехзначное число и на выходе
+//выдает вторую цифру этого числа
+
+Console.WriteLine("Введите трехзначное число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+int ThreeTwoDigit = num1 % 100;
+int TwoDigit = ThreeTwoDigit / 10;
+
+Console.WriteLine(TwoDigit);
+
+
+/*int TwoDigit(int num1)
+{
+    return num1 % 100;
+}
+Console.WriteLine(TwoDigitint(num1));*/
+
+/*int testnumber = 5123;
+int lastDigit = GetlastDigit(testnumber);
+
+Console.WriteLine(lastDigit);*/
