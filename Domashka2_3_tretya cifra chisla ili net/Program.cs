7db10a8884ce1fd41fd67c6153817405ab9aa7ea
Vlad84 <vlad_zaycev_1984@mail.ru>
2022-10-20 00:13:01 +0300

    третья цифра числа если она есть

---
+++ Domashka2_3_tretya cifra chisla ili net/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сщщбщает,
+//что третьей цифры нет
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if(num > 1000)
+{   
+    int i = num;
+    while (i > 1000)
+    {
+        i = i /10;
+    }
+        int ThreeDigit = i % 10;
+        Console.WriteLine(ThreeDigit);
+
+}     
+else
+    if (num > 100)
+   {
+      int ThreeDigit = num % 10;
+      Console.WriteLine(ThreeDigit);  
+   }
+    else
+   {
+    Console.WriteLine("нет третьей цифры");
+   }
+
