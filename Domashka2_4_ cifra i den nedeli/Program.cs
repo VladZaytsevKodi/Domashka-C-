5f503368c48d9a6fa2a084eb2adfb3f8c5462b3b
Vlad84 <vlad_zaycev_1984@mail.ru>
2022-10-19 21:53:18 +0300

    дни недели цифрами и выходные

---
+++ Domashka2_4_ cifra i den nedeli/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, котрая принимает на вход цифру, образющую день недели 
+//и проверяет является ли этот день выходным
+
+
+Console.WriteLine("Введите номер дня недели: ");
+int n = int.Parse(Console.ReadLine()!);
+
+if(n == 1) Console.WriteLine("нет");   
+if(n == 2) Console.WriteLine("нет");  
+if(n == 3) Console.WriteLine("нет");   
+if(n == 4) Console.WriteLine("нет");
+if(n == 5) Console.WriteLine("нет");   
+if(n == 6) Console.WriteLine("да");
+if(n == 7) Console.WriteLine("да"); 
+
+
