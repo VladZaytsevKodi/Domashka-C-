7c407ce88c9e553c140044db873aa5d53f02cc8a
Vlad84 <vlad_zaycev_1984@mail.ru>
2022-10-19 16:09:28 +0300

    удаление 2й цифры из числа

--- Domashka2_2_udalit 2 cifru chisla/Program.cs
+++ Domashka2_2_udalit 2 cifru chisla/Program.cs
@@ -8,11 +8,3 @@ int FirstDigit = Threedigit / 100;
 Console.Write(FirstDigit);
 int ThreeDigit = Threedigit % 10;
 Console.WriteLine(ThreeDigit);
-
-/*Console.WriteLine("Введите трехзначное число: ");
-int num1 = int.Parse(Console.ReadLine()!);
-
-int ThreeTwoDigit = num1 % 100;
-int TwoDigit = ThreeTwoDigit / 10;
-
-Console.WriteLine(TwoDigit);*/
