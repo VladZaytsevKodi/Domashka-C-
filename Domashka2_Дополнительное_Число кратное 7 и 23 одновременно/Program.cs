1352a0b9094b1fae2261853b39d5195fd21e5c11
Vlad84 <vlad_zaycev_1984@mail.ru>
2022-10-19 22:04:41 +0300

    дополнительное из семенара

---
+++ Domashka2_Дополнительное_Число кратное 7 и 23 одновременно/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и проверяет кратно ли оно одновременно 7 и 23
+Console.WriteLine("Введите число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+if(num1 % 7 == 0)
+{
+    if(num1 % 23 == 0)
+    {
+        Console.WriteLine("да");   
+    }   
+}
+else
+{
+    Console.WriteLine("нет");
+}
