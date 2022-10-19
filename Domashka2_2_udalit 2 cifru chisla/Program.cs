33dd33d67018ec3c677858bff7d5329de067cd0f
Vlad84 <vlad_zaycev_1984@mail.ru>
2022-10-19 16:08:24 +0300

    удаление 2й цифры из числа

---
+++ Domashka2_2_udalit 2 cifru chisla/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит слчайное трехзначное число и 
+//удаляет вторую цифру этого числа
+
+int Threedigit = new Random().Next(100, 1000);
+Console.WriteLine(Threedigit);
+
+int FirstDigit = Threedigit / 100;
+Console.Write(FirstDigit);
+int ThreeDigit = Threedigit % 10;
+Console.WriteLine(ThreeDigit);
+
+/*Console.WriteLine("Введите трехзначное число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+int ThreeTwoDigit = num1 % 100;
+int TwoDigit = ThreeTwoDigit / 10;
+
+Console.WriteLine(TwoDigit);*/
